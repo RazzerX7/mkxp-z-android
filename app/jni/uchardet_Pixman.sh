09d745a16514de8532e13b29d76586aa17e4e146
RazzerX7 <39648302+RazzerX7@users.noreply.github.com>
2023-11-28 03:11:14 -0300

    Update uchardet_Pixman.sh

--- app/jni/uchardet_Pixman.sh
+++ app/jni/uchardet_Pixman.sh
@@ -1,11 +1,11 @@
 echo "Downloading uchardet..."
-wget -o https://drive.google.com/u/0/uc?id=1C1HM8fGg6JglDPsBtKv7vlliDkXGt8XW&export=download
+wget https://drive.google.com/u/0/uc?id=1C1HM8fGg6JglDPsBtKv7vlliDkXGt8XW&export=download
 tar -xzf uchardet-v0.0.8.tar.gz
 mv uchardet-v0.0.8 uchardet
 rm -f uchardet-v0.0.8.tar.gz
 
 echo "Downloading Pixman..."
-wget -o https://drive.google.com/u/0/uc?id=1PPmUkNDzZS6x6ptjX_kOLBb6RyKOMtgb&export=download
+wget https://drive.google.com/u/0/uc?id=1PPmUkNDzZS6x6ptjX_kOLBb6RyKOMtgb&export=download
 tar -xzf pixman-pixman-0.42.2.tar.gz
 mv pixman-pixman-0.42.2 Pixman
 rm -f pixman-pixman-0.42.2.tar.gz
