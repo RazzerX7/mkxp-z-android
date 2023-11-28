9fca7d97e34fc62f1c93368d78c6238db51ea5b5
RazzerX7 <39648302+RazzerX7@users.noreply.github.com>
2023-11-28 03:04:21 -0300

    Update uchardet_Pixman.sh

--- app/jni/uchardet_Pixman.sh
+++ app/jni/uchardet_Pixman.sh
@@ -1,11 +1,11 @@
 echo "Downloading uchardet..."
-wget -o https://raw.githubusercontent.com/RazzerX7/mkxp-z-android/master/pixman-pixman-0.42.2.tar.gz
+wget -o https://github.com/RazzerX7/mkxp-z-android/raw/master/pixman-pixman-0.42.2.tar.gz
 tar -xzf uchardet-v0.0.8.tar.gz
 mv uchardet-v0.0.8 uchardet
 rm -f uchardet-v0.0.8.tar.gz
 
 echo "Downloading Pixman..."
-wget -o https://raw.githubusercontent.com/RazzerX7/mkxp-z-android/master/pixman-pixman-0.42.2.tar.gz
+wget -o https://github.com/RazzerX7/mkxp-z-android/raw/master/pixman-pixman-0.42.2.tar.gz
 tar -xzf pixman-pixman-0.42.2.tar.gz
 mv pixman-pixman-0.42.2 Pixman
 rm -f pixman-pixman-0.42.2.tar.gz
